3587174b9934a3ff5348ac78fa8a00adea7043e5
BADARAHMEDSHEIKH <badr313merchant@gmail.com>
2021-03-31 16:16:19 -0700

    index is done

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. Badar Ahmed Sheikh AND
-          HAMZA AHMED SHEIK 
+          HAMZA AHMED SHEIK  badar ahmed sheikh 
           ZZZZZZZZZZZZZZZZZZ
         </p>
         <a
